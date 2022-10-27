1ba020d572c12f1e125a20817ee33ad0ed28a3d5
Carelle Matthews <cmm7635@psu.edu>
2022-10-27 16:15:47 -0400

    Update q2.scala

--- sparklab/q2/q2.scala
+++ sparklab/q2/q2.scala
@@ -1,3 +1,30 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+
+object specificWordLength {
+	def main(args: Array[String]) = {
+		val sc = getSC()
+		val length = doSpecificWordLength(sc)
+		saveit(length)
+	}
+
+	def getSC() = {
+		val conf = new SparkConf().setAppName("swl")
+		val sc = new SparkContext(conf)
+		sc
+	}
+
+	def doSpecificWordLength(sc: SparkContext) = {
+		val input = sc.tetxFile("/datasets/wap")
+		val words = input.flatMap(-.split(" "))
+		val wordLength = words.flatMap(-.split(" "))
+		val kv = wordLength.map(word => (word,1))
+		val length = kv.reduceByKey((x,y) => x+y>1)
+		length
+	}
+	
+	def saveit(length: org.apache.spark.rdd.RDD[(Int, Int)]) = {
+		length.saveAsTextFile("result")
+	}
+}
