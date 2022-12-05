d691ff38dde28b810d2c46194f57b6a508db2eaf
Carelle Matthews <cmm7635@psu.edu>
2022-12-05 18:33:18 -0500

    Update q1.scala

--- sparkhw/q1/q1.scala
+++ sparkhw/q1/q1.scala
@@ -27,8 +27,7 @@ object Q1 extends App {
       val records = noheader.map{x => x.split(",")}
       val outgoing = records.map{record => (records(3), -record(7).toFloat)}
       val incoming = records.map{record => (records(5), record(7).toFloat)}
-      val counts = incoming - outgoing
-
+      val count = incoming - outgoing
       def zeroVal = (0.0,0.0)
       def func = (state: (Float, Float), v: (Float, Float)) = state._1 + v._1, state._2 + v._2)
       val result = both.aggregateByKey(zeroVal)(func, func)
