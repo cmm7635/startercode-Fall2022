cd3a776b091a73322298038bcff6790cb6e3001f
Carelle Matthews <cmm7635@psu.edu>
2022-12-05 18:31:14 -0500

    Update q1.scala

--- sparkhw/q1/q1.scala
+++ sparkhw/q1/q1.scala
@@ -4,7 +4,41 @@ import org.apache.spark.SparkContext._
 
 object Q1 extends App {
   def main(args: Array[String]) = {
-    // call your code using spark-submit nameofjarfile.jar commandlinearg
-    val cmd_arg = args(0) // this is the commandlinearg
+    val sc = getSC()
+    val input = getRDD(sc)
+    val pCount = popularAirport(input)
+    saveit(pCount) 
   }
+
+  def getSC() = {
+    val conf = new SparkConf().setAPPName("Q1")
+    val sc = new SparkContent(conf)
+    sc
+  }
+
+  def getRDD(sc: SparkContext) = {
+    val input = sc.textFile("/datasets/flight")
+    input
+  } 
+
+  def popularAirport(input: RDD[String]) = {
+      val flights = sc.textfile("/datasets/flight")
+      val noheader = flights.filter{line => !line.startsWith("ITIN_ID")}
+      val records = noheader.map{x => x.split(",")}
+      val outgoing = records.map{record => (records(3), -record(7).toFloat)}
+      val incoming = records.map{record => (records(5), record(7).toFloat)}
+      val counts = incoming - outgoing
+
+      def zeroVal = (0.0,0.0)
+      def func = (state: (Float, Float), v: (Float, Float)) = state._1 + v._1, state._2 + v._2)
+      val result = both.aggregateByKey(zeroVal)(func, func)
+      val pCount = result.filter{case (airport, (incoming, outgoing)) => incoming - outgoing >= x}
+      pCount
+    }
+
+  def saveit(pCount: org.apache.spark.rdd.RDD[(String, Int)} = {
+      pCount.saveAsTextFile(result, "sparkhw-q1")
+  }
+
+  val cmd_arg = args(0)
 }
